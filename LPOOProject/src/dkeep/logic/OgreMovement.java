9e38a0889e20655789fe6bb8f2ac44987ec1c289
ZeLuis <up201504880@fe.up.pt>
2017-03-26 06:53:02 +0100

    improvements

--- LPOOProject/src/dkeep/logic/OgreMovement.java
+++ LPOOProject/src/dkeep/logic/OgreMovement.java
@@ -3,8 +3,40 @@ package dkeep.logic;
 import java.util.Random;
 
 public class OgreMovement implements MovementStrategy{
-	
-	
+
+	public boolean direction2(char[][] table, int X, int Y, boolean valid){
+		if(table[X][Y+1] != 'X' && table[X][Y+1] != 'I' && table[X][Y+1] != 'S'){
+			Y += 1;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean direction4(char[][] table, int X, int Y, boolean valid){
+		if(table[X-1][Y] != 'X' && table[X-1][Y] != 'I' && table[X-1][Y] != 'S'){
+			X -= 1;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean direction6(char[][] table, int X, int Y, boolean valid){
+		if(table[X+1][Y] != 'X' && table[X][Y+1] != 'I' && table[X][Y+1] != 'S'){
+			X += 1;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean direction8(char[][] table, int X, int Y, boolean valid){
+		if(table[X][Y-1] != 'X' && table[X][Y-1] != 'I' && table[X][Y-1] != 'S'){
+			Y -= 1;
+			return true;
+		}
+		return false;
+	}
+
+
 	public int getDirection(char[][] table, int X, int Y){
 		Random rand = new Random();
 		boolean valid = false;
@@ -12,34 +44,21 @@ public class OgreMovement implements MovementStrategy{
 		while(!valid){
 			direction = (rand.nextInt(4) + 1)*2;
 			if(direction == 2){
-				if(table[X][Y+1] != 'X' && table[X][Y+1] != 'I' && table[X][Y+1] != 'S'){
-					Y += 1;
-					valid = true;
-				}
-
+				valid = direction2(table,X,Y,valid);
 			}
 			else if(direction == 4){
-				if(table[X-1][Y] != 'X' && table[X-1][Y] != 'I' && table[X-1][Y] != 'S'){
-					X -= 1;
-					valid = true;
-				}
+				valid = direction4(table,X,Y,valid);
 			}
 			else if (direction == 6){
-				if(table[X+1][Y] != 'X' && table[X][Y+1] != 'I' && table[X][Y+1] != 'S'){
-					X += 1;
-					valid = true;
-				}
+				valid = direction6(table,X,Y,valid);
 			}
 			else if(direction == 8){
-				if(table[X][Y-1] != 'X' && table[X][Y-1] != 'I' && table[X][Y-1] != 'S'){
-					Y -= 1;
-					valid = true;
-				}
+				valid = direction8(table,X,Y,valid);
 			}
 		}
 		return direction;
 	}
-	
-	
-	
+
+
+
 }
